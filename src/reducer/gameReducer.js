7dc7cf0a5a15ee3689c52a7bea2d20d73d5eb0a6
Ewelina Łabas <ewelina.labas@gmail.com>
2019-03-15 17:57:00 +0100

    Add checking for winner for rows

--- src/reducer/gameReducer.js
+++ src/reducer/gameReducer.js
@@ -1,24 +1,45 @@
+import { getKeyByValue } from '../utils/object';
+
 const initialState = {
-    board: [
-        [{mark: null}, {mark: null}, {mark: null}],
-        [{mark: null}, {mark: null}, {mark: null}],
-        [{mark: null}, {mark: null}, {mark: null}]
-    ],
-    currentTurn: true
+  board: [
+    [{ mark: null }, { mark: null }, { mark: null }],
+    [{ mark: null }, { mark: null }, { mark: null }],
+    [{ mark: null }, { mark: null }, { mark: null }]
+  ],
+  currentTurn: true
 }
 
 const updateField = (row, column, mark, state) => {
-    let newBoard = [...state.board]
-    newBoard[row][column].mark = mark
-    return {...state, board: newBoard, currentTurn: !state.currentTurn}
+  let newBoard = [...state.board]
+  newBoard[row][column].mark = mark
+  validateWin(newBoard)
+  return { ...state, board: newBoard, currentTurn: !state.currentTurn }
 }
 
 export const gameReducer = (state = initialState, action) => {
-    console.log('action', action);
-    switch (action.type) {
-        case 'UPDATE_BOARD':
-            return updateField(action.row, action.column, action.mark, state);
-        default: 
-            return state;
-    }
+  console.log('action', action);
+  switch (action.type) {
+    case 'UPDATE_BOARD':
+      return updateField(action.row, action.column, action.mark, state);
+    default:
+      return state;
+  }
+}
+
+const checkLine = line => {
+  const countedMarks =
+    line
+      .map(f => f.mark)
+      .filter(m => m != null)
+      .reduce((marks, m) => {
+        marks[m] ? marks[m] += 1 : marks[m] = 1;
+        return marks
+      }, {});
+
+  return getKeyByValue(countedMarks, 3)
+}
+
+const validateWin = board => {
+  const winner = board.map(checkLine).filter(el => el)[0]
+  console.log(`Winner is ${winner}`)
 }
